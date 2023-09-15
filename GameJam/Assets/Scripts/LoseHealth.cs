4fb2856b56f7409f9c5c12a4aaf27c8e71e1b90e
MatthiusM <91685851+MatthiusM@users.noreply.github.com>
2023-09-15 14:30:07 +0100

    health script

--- GameJam/Assets/Scripts/LoseHealth.cs
+++ GameJam/Assets/Scripts/LoseHealth.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LoseHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject[] health;
+    public static int amount;
+
+    private void Start()
     {
-        
+        amount = health.Length;
+        StartCoroutine(Damage());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (amount <= 0)
+        {
+            //exit scene
+        }
+    }
+
+    IEnumerator Damage()
+    {
+        while (true)
+        {
+            loseHealth();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void loseHealth()
+    {
+        health[amount - 1].SetActive(false);
+        amount -= 1;
     }
 }
